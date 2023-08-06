884e9d906a7147b47721e97b3f5b90052762f0b2
Leandro Artilha <leandro.artilha@gmail.com>
2023-08-06 15:50:38 -0300

    first commit

---
+++ alfasoftexercicio/alfasoftexercicio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace alfasoftexercicio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
