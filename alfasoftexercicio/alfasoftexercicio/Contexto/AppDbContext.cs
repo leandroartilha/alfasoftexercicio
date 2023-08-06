6b49db8f5d68b5ff8e0fe84f8b3bac06b108a8e8
Leandro Artilha <leandro.artilha@gmail.com>
2023-08-06 16:05:01 -0300

    databse configuration

---
+++ alfasoftexercicio/alfasoftexercicio/Contexto/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace alfasoftexercicio.Contexto
+{
+    public class AppDbContext : DbContext
+    {
+        
+    }
+}
+
--- alfasoftexercicio/alfasoftexercicio/Program.cs
+++ alfasoftexercicio/alfasoftexercicio/Program.cs
@@ -1,4 +1,8 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System;
+using alfasoftexercicio.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +16,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
